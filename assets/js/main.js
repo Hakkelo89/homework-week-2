e5f1a49efc3068cc37120bce4b4bef52f294d3ca
Hakkelo89 <78032311+Hakkelo89@users.noreply.github.com>
2021-05-02 22:05:05 +0100

    index.html updated and js file created

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+// Create a request variable and assing a new XMLHttpRequest object to it.
+const request = new XMLHttpRequest();
+
+//Open a new connection, using the GET request on the API endpoint.
+request.open("GET", "https://api.github.com/users/Hakkelo89/repos", true);
+
+request.onload = function () {
+  //Begin accesing JSON data here
+  const data = JSON.parse(this.response);
+
+  const repoHTML =
+    "<h2>" +
+    "My Projects" +
+    "</h2>" +
+    "<section>" +
+    '<div id="' +
+    "icon-wrapper" +
+    '">';
+  $.each(data, function (i, repo) {
+    repoHTML += "<a target=" + "_blank" + "href=" + repo.html_url + ">";
+    repoHTML +=
+      '<div class="' +
+      "icons" +
+      '">' +
+      '<div class="' +
+      "icon-slide-container" +
+      '">';
+    repoHTML +=
+      '<img class="' +
+      'slide-icon"' +
+      'alt="' +
+      "Project Icon" +
+      'height="' +
+      '100"' +
+      'src="' +
+      repo.avatar_url +
+      '"/>';
+    repoHTML += "</div>" + "</div>" + "</a>";
+  });
+};
+
+const repoCards = document.getElementsByClassName("projectSlider");
+
+repoCards.HTML = "";
+
+repoCards.HTML = repoHTML;
+
+//Send request
+request.send();
