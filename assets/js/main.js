64ea62e95bca3d7179367254ccffbed6ed998b95
Hakkelo89 <78032311+Hakkelo89@users.noreply.github.com>
2021-05-13 15:03:47 +0100

    Cv Updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,43 +8,63 @@ request.open("GET", "https://api.github.com/users/Hakkelo89/repos", true);
 request.onload = function () {
   //Begin accesing JSON data here
   const data = JSON.parse(this.response);
-  var repoCards = document.getElementById("repos");
-  repoCards.innerHTML = "";
+  var repoCards = $("#repos");
+  debugger;
 
   console.log(data);
 
-  repoCards.innerHTML +=
-    "<h2>" +
-    "My Projects" +
-    "</h2>" +
-    "<section>" +
-    '<div id="' +
-    "icon-wrapper" +
-    '">';
-  $.each(data, function (i, repo) {
-    console.log(repo.html_url);
-    repoCards.innerHTML +=
-      "<a target=" + "_blank" + "href=" + repo.html_url + ">";
-    repoCards.innerHTML +=
-      '<div class="' +
-      "icons" +
-      '">' +
-      '<div class="' +
-      "icon-slide-container" +
-      '">';
-    repoCards.innerHTML +=
-      '<img class="' +
-      'slide-icon"' +
-      'alt="' +
-      "Project Icon" +
-      'height="' +
-      '100"' +
-      'src="' +
-      repo.avatar_url +
-      '"/>';
-    repoCards.innerHTML += "</div>" + "</div>" + "</a>";
+  var h2 = document.createElement("h2");
+  h2.innerHTML = "My Projects";
+  repoCards.append(h2);
+
+  var section = document.createElement("section");
+
+  var projectsContainer = document.createElement("div");
+  projectsContainer.id = "icon-wrapper";
+
+  /* data.array.forEach(element => {
+    var l = document.createElement('a');
+    l.href = value.html_url;
+
+    var i = document.createElement('div');
+    i.className = "icons";
+    var s = document.createElement('div');
+    s.className = 'icon-slide-container';
+
+    var imgS = document.createElement('img');
+    imgS.className = "slide-icon";
+    imgS.alt = "Project Icon";
+    imgS.style.height = "100px";
+    imgS.src = value.owner.avatar_url;
+
+    s.appendChild(imgS);
+    i.appendChild(s);
+    l.appendChild(i);
+    d.appendChild(l);
+    
+  });*/
+
+  $.each(data, function (index, value) {
+    const project = `<a
+    target="_blank"
+    href="${value.html_url}"
+  >
+    <div class="icons">
+      <div class="icon-slide-container">
+        <img
+          class="slide-icon"
+          alt="Project Icon"
+          height="100"
+          src="../images/project_${index}.png"
+        />
+      </div>
+    </div>
+  </a>`;
+
+    projectsContainer.appendChild(project);
   });
-  repoCards.innerHTML += "</section>";
+
+  repoCards.append(section);
 };
 
 //Send request
